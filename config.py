34c05f086117e74d3adab4aa3336d89bd306515a
Gracy <gracybird@pm.me>
2023-05-21 14:36:17 -0700

    classes files

---
+++ config.py
@@ -0,0 +1,12 @@
+#
+# Configuration for ALiceBot
+#
+
+# sybol that proceeds all commands
+prefix = '.'
+
+# bot token from discord setup
+token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+# prefix for database filenames
+db_prefix = 'db_'
